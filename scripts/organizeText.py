853b5815b2952fa5fdbc3de14b24650ef4489fbc
Jocelyn MEYRON <Jocelyn.Meyron@insa-lyon.fr>
2021-01-12 15:41:18 +0100

    Reorganize code, create links to each mission in the index

--- scripts/organizeText.py
+++ scripts/organizeText.py
@@ -2,103 +2,91 @@
 
 import os
 import re
-import sys
 
 SPEAKERS = {
-    "a2b": "2B",
-    "a9s": "9S",
-    "a2": "A2",
-    "op60": "Operator 6O",
-    "pod042": "Pod 042",
-    "pod153": "Pod 153",
-    "a1d": "1D",
-    "a4b": "4B",
-    "telo": "TODO",
-    "eng": "Engels",
+    "a2b"        : "2B",
+    "a7b"        : "7B",
+    "a8b"        : "8B",
+    "a9s"        : "9S",
+    "a11s"       : "11S",
+    "a2"         : "A2",
+    "a1d"        : "1D",
+    "a4b"        : "4B",
+    "op60"       : "Operator 6O",
+    "op210"      : "Operator 21O",
+    "pod042"     : "Pod 042",
+    "pod153"     : "Pod 153",
+    "ane"        : "Anemone",
+    "resi"       : "Resistance Member",
+    "resiman"    : "Resistance Member (man)",
+    "resiwoman"  : "Resistance Member (woman)",
+    "resiwoman2" : "Resistance Member (woman)",
+    "pascal"     : "Pascal",
+    "eng"        : "Engels",
+    "adam"       : "Adam",
+    "eve"        : "Eve",
 }
 
-def parseId(id):
-    splitted = id.split("_")
-    if len(splitted) != 5:
-        print(id)
-        # TODO: check
-        return None
+ALL_MISSIONS_FILE = "scripts/data/ALL_MISSIONS.txt"
+assert os.path.isfile(ALL_MISSIONS_FILE)
 
-    mission = splitted[0]
-    section = splitted[1]
-    scene = splitted[2]
-    number = splitted[3]
-    speaker = splitted[4]
+class LineID(object):
+    def __init__(self, s):
+        self._parse(s)
 
-    return {
-        "mission": mission,
-        "section": section,
-        "scene"  : scene,
-        "number" : number,
-        "speaker": speaker,
-    }
+    def isValid(self):
+        if self.mission[0] != "M":
+            return False
+        if self.section[0] != "S":
+            return False
+        if self.scene[0] not in ["G", "H", "S"]:
+            return False
 
-def scenePrefixToNumber(scene):
-    s = scene[0]
-    assert s in ["S", "G", "H"]
-    if s == "S":
-        return 0
-    elif s == "G":
-        return 1
-    elif s == "H":
-        return 2
+        return True
 
-def idKeyFunction(id):
-    mission = id["mission"]
-    assert mission[0] == "M", "{}".format(mission)
-    section = id["section"]
-    assert section[0] == "S", "{}".format(section)
-    scene = id["scene"]
-    assert scene[0] in ["G", "H", "S"]
-    number = id["number"]
+    def comparator(self):
+        assert self.isValid()
 
-    return (int(mission[1:]), int(section[1:]), scenePrefixToNumber(scene), int(scene[1:]), int(number))
+        return (int(self.mission[1:]), int(self.section[1:]), self._scenePrefixToNumber(), int(self.scene[1:]), int(self.number))
 
-def parseFile(filename, parsedText):
-    contents = None
-    with open(filename, "r") as f:
-        contents = f.readlines()
-    if contents is None:
-        return
+    def getSpeaker(self):
+        if self.speaker not in SPEAKERS.keys():
+            return "UNKNOWN ({})".format(self.speaker)
+        return SPEAKERS[self.speaker]
 
-    contents = [ line.strip() for line in contents ]
-    i = 0
-    data = {}
-    while i < len(contents):
-        line = contents[i]
+    def __str__(self):
+        s = ""
+        s += "mission={},".format(self.mission)
+        s += "section={},".format(self.section)
+        s += "scene={},".format(self.scene)
+        s += "number={},".format(self.number)
+        s += "speaker{}".format(self.speaker)
 
-        if line.startswith("ID"):
-            id = contents[i][4:]
-            jp = contents[i+1][4:]
-            en = contents[i+2][4:]
-            parsedId = parseId(id)
-            data[id] = (parsedId, jp, en, filename)
-            i += 3
-        else:
-            i += 1
+        return s
 
-    # data = dict(sorted(data.items(), key=lambda item: idKeyFunction(item[1][0])))
-    # print("\n".join([k for k, v in data.items()]))
+    def _parse(self, s):
+        self.raw_id = s
 
-    return {**parsedText, **data}
+        splitted = s.split("_")
+        if len(splitted) != 5:
+            raise ValueError("{} is not a valid Id".format(s))
 
-def exportText(text, fname, filter=None):
-    with open(fname, "w") as f:
-        for id, v in text.items():
-            # if (filter is not None) and (filter not in id):
-            #     continue
-            if (filter is not None) and (re.match(filter, id) is None):
-                continue
+        self.mission = splitted[0]
+        self.section = splitted[1]
+        self.scene = splitted[2]
+        self.number = splitted[3]
+        self.speaker = splitted[4]
 
-            idParsed, jp, en, filename = v
-            f.write("filename: {}\n".format(filename))
-            f.write("ID: {}\n".format(id))
-            f.write("JP: {}\nEN: {}\n\n".format(jp, en))
+    def _scenePrefixToNumber(self):
+        assert self.isValid()
+
+        s = self.scene[0]
+        if s == "S":
+            return 0
+        elif s == "G":
+            return 1
+        elif s == "H":
+            return 2
 
 HTML_TEMPLATE = """
 <!DOCTYPE html>
@@ -106,13 +94,17 @@ HTML_TEMPLATE = """
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="stylesheet" href="css/yorha.min.css"/>
-        <title>YoRHa</title>
+        <link rel="stylesheet" href="../css/yorha.min.css"/>
+        <title>Nier:Automata - {title}</title>
     </head>
     <body>
         <h1>Nier:Automata</h1>
+        <h2>{title}</h2>
 
-        {}
+        <p>
+            <a href="../index.html">Home</a>
+        </p>
+        {text}
     </body>
 </html>
 """
@@ -127,38 +119,99 @@ LINE_TEMPLATE = """
         </figure>
 """
 
-def exportHtml(text, fname, filter=None):
-    contents = ""
-    for id, v in text.items():
-        # if (filter is not None) and (filter not in id):
-        #     continue
-        if (filter is not None) and (re.match(filter, id) is None):
-            continue
+class TextDump(object):
+    def __init__(self):
+        self.data = {}
 
-        idParsed, jp, en, filename = v
-        contents += LINE_TEMPLATE.format(caption=SPEAKERS[idParsed["speaker"]], japanese=jp, english=en)
-    contents = HTML_TEMPLATE.format(contents)
+    def __str__(self):
+        return "\n".join([k for k, v in self.data.items()])
 
-    with open(fname, "w") as f:
-        f.write(contents)
+    def sort(self):
+        self.data = dict(sorted(self.data.items(), key=lambda item: item[1][0].comparator()))
+
+    def parseFile(self, filename):
+        assert os.path.isfile(filename)
+
+        with open(filename, "r") as f:
+            contents = f.readlines()
+
+        contents = [ line.strip() for line in contents ]
+        i = 0
+        data = {}
+        while i < len(contents):
+            line = contents[i]
+
+            if line.startswith("ID"):
+                id = contents[i][4:]
+                jp = contents[i+1][4:]
+                en = contents[i+2][4:]
+
+                self.data[id] = (LineID(id), jp, en, filename)
+                i += 3
+            else:
+                i += 1
+
+    def exportText(self, fname, filter=None, verbose=False):
+        if verbose:
+            print("Exporting to {}...".format(fname))
+
+        with open(fname, "w") as f:
+            for id, v in self.data.items():
+                if (filter is not None) and (re.match(filter, id) is None):
+                    continue
+
+                idParsed, jp, en, filename = v
+                f.write("filename: {}\n".format(filename))
+                f.write("ID: {}\n".format(id))
+                f.write("JP: {}\nEN: {}\n\n".format(jp, en))
+
+    def exportHtml(self, fname, title, filter=None, verbose=False):
+        if verbose:
+            print("Exporting to {}...".format(fname))
+
+        contents = ""
+        for id, v in self.data.items():
+            if (filter is not None) and (re.match(filter, id) is None):
+                continue
+
+            idParsed, jp, en, filename = v
+            contents += LINE_TEMPLATE.format(caption=idParsed.getSpeaker(), japanese=jp, english=en)
+        contents = HTML_TEMPLATE.format(title=title, text=contents)
+
+        with open(fname, "w") as f:
+            f.write(contents)
 
 if __name__ == "__main__":
-    DIRS = ["data/ph1", "data/ph2"]
+    # Parse missions file
+    with open(ALL_MISSIONS_FILE, "r") as f:
+        ALL_MISSIONS = f.readlines()
+        ALL_MISSIONS = [ mission.strip() for mission in ALL_MISSIONS ]
 
-    parsedText = {}
+    # Parse all text
+    DIRS = ["data/ph1", "data/ph2", "data/ph3", "data/ph4", "data/phf"]
+
+    textDump = TextDump()
     for d in DIRS:
+        print("Reading from directory {}:".format(d))
+        assert os.path.isdir(d)
         for root, dir, files in os.walk(d):
             for name in files:
                 filename = os.path.join(root, name)
-                print(filename)
-                parsedText = parseFile(filename, parsedText)
+                print("\t- parsing {}...".format(filename))
+                textDump.parseFile(filename)
 
-    parsedText = dict(sorted(parsedText.items(), key=lambda item: idKeyFunction(item[1][0])))
-    print("\n".join([k for k, v in parsedText.items()]))
+    textDump.sort()
 
-    # First mission, first route (A)
-    # exportText(parsedText, "output/mission0010.txt", filter="M0010")
-    exportText(parsedText, "output/mission0010.txt", filter=r"M0010_.+_.+_[01]\d+_.")
-    exportHtml(parsedText, "website/test.html", filter=r"M0010_.+_.+_[01]\d+_.")
+    # Export
+    # First route, first mission
+    # textDump.exportText("output/mission0010.txt",
+    #                     filter=r"M0010_.+_.+_[01]\d+_.", verbose=True)
+    # textDump.exportHtml("website/missions/0010.html", title="Misssion 0010",
+    #                     filter=r"M0010_.+_.+_[01]\d+_.", verbose=True)
 
-    # exportText(parsedText, "output/mission1020.txt", filter=r"M1020_S\d+_.+_[01]\d+_.")
+    # All missions for the first route
+    # TODO: check if it is really only the first route
+    for mission in ALL_MISSIONS:
+        textDump.exportHtml("website/missions/{}.html".format(mission[1:]),
+                            title="Mission {}".format(mission[1:]),
+                            filter=r"{}_S\d+_.+_[01]\d+_.".format(mission), verbose=True)
