8f69a406546d59d294d1979e6a751e80d90d41da
Jocelyn MEYRON <Jocelyn.Meyron@insa-lyon.fr>
2021-01-14 17:34:56 +0100

    Add a condition inside the publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,10 +8,14 @@ cd website
 
 rm -rf .git
 git init
-# git remote add origin git@github.com:nyorem/NierAutomataTextDump.git
 git checkout -b gh-pages
 git add -A
 git commit -m "deploy"
-# git push -f origin gh-pages
+
+# If we are not inside a GitHub action, do the following
+if [ -z ${CI+x} ]; then
+    git remote add origin git@github.com:nyorem/NierAutomataTextDump.git
+    git push -f origin gh-pages
+fi
 
 cd -
