d38764d092a708410728f2c42cb39acde192a09f
Jocelyn MEYRON <Jocelyn.Meyron@insa-lyon.fr>
2021-01-14 17:31:54 +0100

    try to use github-push-action

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,10 +8,10 @@ cd website
 
 rm -rf .git
 git init
-git remote add origin git@github.com:nyorem/NierAutomataTextDump.git
+# git remote add origin git@github.com:nyorem/NierAutomataTextDump.git
 git checkout -b gh-pages
 git add -A
 git commit -m "deploy"
-git push -f origin gh-pages
+# git push -f origin gh-pages
 
 cd -
